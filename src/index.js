9c637a1173ecf82fb15b5d5cd84dece3df0e9de3
Justin Yapjoco <94045038+Shisu123@users.noreply.github.com>
2025-03-19 21:59:29 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not set
 app.use(express.json({ limit: "50mb"}));    // Middleware
 app.use(cookieParser());    // Extracts json data out of body
 app.use(cors({
-    origin: ["http://localhost:5173", "https://malarkyfrontend.onrender.com"],
+    origin: ["http://localhost:5173", "https://main.d1tmmanuj6ct5v.amplifyapp.com"],
     credentials: true,
 }));         
 
