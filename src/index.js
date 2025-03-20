bef07a31ddd01b99b6651ff3ac811c66582f902b
Justin Yapjoco <94045038+Shisu123@users.noreply.github.com>
2025-03-19 21:45:20 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,19 +10,19 @@ import { app, server } from './lib/socket.js';      // Socket.io
 
 dotenv.config();
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT || 3000; // Default to 3000 if PORT is not set
 
 app.use(express.json({ limit: "50mb"}));    // Middleware
 app.use(cookieParser());    // Extracts json data out of body
-app.use(cors( {
-    origin: "http://localhost:5173",
-    credentials: true,       // Allows cookies to be sent with req.
-}));            
+app.use(cors({
+    origin: ["http://localhost:5173", "https://malarkyfrontend.onrender.com"],
+    credentials: true,
+}));         
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, () => {
-    console.log('Server is running on PORT: ' + PORT);
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on PORT: ${PORT}`);
     connectDB();
 });
